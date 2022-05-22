915e27591dbbd121a6be42a0eb0f10ea3543fa00
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-05-22 17:20:39 +0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Gokhan Bozkurt on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
